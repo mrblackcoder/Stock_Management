55c112be12f0a8a851d344a53ac46fb7fd4e91cc
mrblackcoder <mehmet.boynikoglu@stu.fsm.edu.tr>
2026-07-22 04:02:15 +0300

    test: fix JWT test property keys and re-enable context load test

--- src/test/java/com/ims/stockmanagement/StockManagementApplicationTests.java
+++ src/test/java/com/ims/stockmanagement/StockManagementApplicationTests.java
@@ -1,13 +1,11 @@
 package com.ims.stockmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Disabled("Integration test - requires full configuration. Run manually.")
 class StockManagementApplicationTests {
 
     @Test
